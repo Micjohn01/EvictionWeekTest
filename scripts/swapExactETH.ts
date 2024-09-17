bd0206aed8a30927555ee5590f55c9765facbbcb
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2024-09-17 17:38:38 +0100

    interaction images added

--- scripts/swapExactETH.ts
+++ scripts/swapExactETH.ts
@@ -3,7 +3,9 @@ import { ethers } from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 async function main() {
-    const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+
+    //Get the Uniswap Router and Tokens
+    const UNISWAP_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
     const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
@@ -16,21 +18,22 @@ async function main() {
     const USDC_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
     const WETH_Contract = await ethers.getContractAt("IERC20", WETH);
 
-    const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS, impersonatedSigner);
-    // const amountOut = 
+    const ROUTER = await ethers.getContractAt("IUniswapV2Router", UNISWAP_ADDRESS, impersonatedSigner);
+     
 
     const amountOutMin = ethers.parseUnits("0", 18);
 
-    const usdcBal = await USDC_Contract.balanceOf(impersonatedSigner.address);
-    const wethBal = await WETH_Contract.balanceOf(impersonatedSigner.address);
-    const ethBal = await ethers.provider.getBalance(impersonatedSigner.address);
+    const usdcBalance = await USDC_Contract.balanceOf(impersonatedSigner.address);
+    const wethBalance = await WETH_Contract.balanceOf(impersonatedSigner.address);
+    const ethBalance = await ethers.provider.getBalance(impersonatedSigner.address);
     const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
 
-    console.log("=========================================================");
-    console.log("usdc balance before swap", Number(usdcBal));
-    console.log("weth balance before swap", Number(wethBal));
-    console.log("eth balance before swap", Number(ethBal));
-    console.log("=========================================================");
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    console.log("usdc balance before swap", Number(usdcBalance));
+    console.log("weth balance before swap", Number(wethBalance));
+    console.log("eth balance before swap", Number(ethBalance));
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    
 
 
     const trx = await ROUTER.swapExactETHForTokens(
@@ -42,18 +45,16 @@ async function main() {
     const receipt = await trx.wait()
     console.log({ receipt })
 
-    const usdcBalAfter = await USDC_Contract.balanceOf(impersonatedSigner.address);
-    const wethBalAfter = await WETH_Contract.balanceOf(impersonatedSigner.address);
-    const ethBalAfter = await ethers.provider.getBalance(impersonatedSigner.address);
-    console.log("=========================================================");
-    console.log("usdc balance after swap", Number(usdcBalAfter));
-    console.log("weth balance after swap", Number(wethBalAfter));
-    console.log("eth balance after swap", Number(ethBalAfter));
-    console.log("=========================================================");
+    const usdcBalanceAfter = await USDC_Contract.balanceOf(impersonatedSigner.address);
+    const wethBalanceAfter = await WETH_Contract.balanceOf(impersonatedSigner.address);
+    const ethBalanceAfter = await ethers.provider.getBalance(impersonatedSigner.address);
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    console.log("usdc balance after swap", Number(usdcBalanceAfter));
+    console.log("weth balance after swap", Number(wethBalanceAfter));
+    console.log("eth balance after swap", Number(ethBalanceAfter));
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
--- scripts/swapTokens.ts
+++ scripts/swapTokens.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 async function main() {
-    const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const UNISWAP_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
     const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
@@ -14,7 +14,7 @@ async function main() {
     const USDC_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
     const WETH_Contract = await ethers.getContractAt("IERC20", WETH);
 
-    const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS, impersonatedSigner);
+    const ROUTER = await ethers.getContractAt("IUniswapV2Router", UNISWAP_ADDRESS, impersonatedSigner);
 
     const amountOut = ethers.parseUnits("0.5", 18);
     const [amountIn] = await ROUTER.getAmountsIn(amountOut, [USDC, WETH]);
@@ -22,16 +22,16 @@ async function main() {
     let amountInMax = ethers.parseUnits(amountIn.toString(), 18);;
     await USDC_Contract.approve(ROUTER, amountInMax);
 
-    const usdcBal = await USDC_Contract.balanceOf(impersonatedSigner.address);
-    const wethBal = await WETH_Contract.balanceOf(impersonatedSigner.address);
-    const ethBal = await ethers.provider.getBalance(impersonatedSigner.address);
+    const usdcBalance = await USDC_Contract.balanceOf(impersonatedSigner.address);
+    const wethBalance = await WETH_Contract.balanceOf(impersonatedSigner.address);
+    const ethBalance = await ethers.provider.getBalance(impersonatedSigner.address);
     const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
 
-    console.log("=========================================================");
-    console.log("usdc balance before swap", Number(usdcBal));
-    console.log("weth balance before swap", Number(wethBal));
-    console.log("Eth balance before swap", Number(ethBal));
-    console.log("=========================================================");
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    console.log("usdc balance before swap", Number(usdcBalance));
+    console.log("weth balance before swap", Number(wethBalance));
+    console.log("Eth balance before swap", Number(ethBalance));
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
     const trx = await ROUTER.swapTokensForExactETH(
         amountOut,
@@ -47,14 +47,12 @@ async function main() {
     const wethBalAfter = await WETH_Contract.balanceOf(impersonatedSigner.address);
     const ethBalAfter = await ethers.provider.getBalance(impersonatedSigner.address);
 
-    console.log("=========================================================");
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     console.log("usdc balance after swap", Number(usdcBalAfter));
     console.log("weth balance after swap", Number(wethBalAfter));
     console.log("Eth balance after swap", Number(ethBalAfter));
-    console.log("=========================================================");
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 }
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
